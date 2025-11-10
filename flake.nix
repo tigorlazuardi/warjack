a61d267f24171036f905f11a5d7a2bf747f30d2e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-11-10 22:45:04 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              bun
+            ];
+            shellHook = ''
+              export PROJECT_DIR=$(git rev-parse --show-toplevel)
+              export DATABASE_DIR="''${PROJECT_DIR}/artifacts/database"
+              export DATABASE_URL="sqlite:///''${DATABASE_DIR}/dev.db"
+
+              mkdir -p "''${DATABASE_DIR}"
+            '';
+          };
+          packages.default = pkgs.hello;
+        };
+    };
+}
